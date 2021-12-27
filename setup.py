62770411f281ac97239bbedca05d9dd3dc66bde2
BuildTools <unconfigured@null.spigotmc.org>
2021-12-27 21:21:55 +0300

    Vox belalısı

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+#with open('requirements.txt') as f:
+#    required = f.read().splitlines()
 
 if __name__ == "__main__":
     setup(
-        install_requires=required,
+        #install_requires=required,
     )
