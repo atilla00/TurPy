e69f7dda178973aeaba7f8204611020f297397c7
BuildTools <unconfigured@null.spigotmc.org>
2021-12-28 22:41:14 +0300

    Execute ORDER 11!

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
 from setuptools import setup
 
-with open('requirements.txt') as file:
-    required = file.read().splitlines()
-
 if __name__ == "__main__":
-    setup(
-        install_requires=required
-    )
+    setup()
