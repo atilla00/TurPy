55f76f565f32739157635e4288710d0002d7e042
BuildTools <unconfigured@null.spigotmc.org>
2022-04-11 03:51:45 +0300

    added TransformerClassifier

--- src/turpy/models/_deep.py
+++ src/turpy/models/_deep.py
@@ -1,8 +1,3 @@
-try:
-    import torch
-except ModuleNotFoundError:
-    raise ImportError("Please install Pytorch beforehand.")
-
 import numpy as np
 import pandas as pd
 from sklearn.utils.multiclass import type_of_target
